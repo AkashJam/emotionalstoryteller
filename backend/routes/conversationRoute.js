33ebe526911cf4ce22d17bcd1144a067301a02bd
AkashJam <akashaloysius.james@gmail.com>
2020-12-17 15:09:11 +0100

    added dialogflow api

--- backend/routes/conversationRoute.js
+++ backend/routes/conversationRoute.js
@@ -3,8 +3,15 @@ const router = require('express').Router();
 const conversationService = require('../services/conversationService')
 
 router.get("/start", async (req, res, next) => {
-    const welcomeMessage = await conversationService.startConversation();
-    res.json(welcomeMessage);
+    const Message = await conversationService.Conversation('hi');
+    res.json({response: `${Message}`, context:{name: "OPEN-CONV"}, imgurl: "", suggestions: ["Yes", "No"]});
+
+});
+
+router.get("/continue", async (req, res, next) => {
+    var userreply = req.body.query;
+    const Message = await conversationService.Conversation(userreply);
+    res.json({response: `${Message}`, context:{name: "OPEN-CONV"}, imgurl: "", suggestions: ["Yes", "No"]});
 
 });
 
--- backend/services/conversationService.js
+++ backend/services/conversationService.js
@@ -1,10 +1,38 @@
 const conversationDAO = require('../DAO/conversationDAO');
+const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
+
+path = __dirname
+process.env.GOOGLE_APPLICATION_CREDENTIALS = `${path}/chatbot-test-bicu-96c40f63fb5b.json`;
+const sessionId = uuid.v4();
+const projectId = 'chatbot-test-bicu'
 
 module.exports = {
-    startConversation: () => {   
-        return true; 
+    Conversation: async (query) => {   
+        // Create a new session
+        const sessionClient = new dialogflow.SessionsClient();
+        const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+        // The text query request.
+        const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+            // The query to send to the dialogflow agent
+            text: query,
+            // The language used by the client (en-US)
+            languageCode: 'en-US',
+            }, 
+        }, 
+        };
+    
+        // Send request and log result
+        const responses = await sessionClient.detectIntent(request);
+        console.log('Detected intent');
+        const result = responses[0].queryResult;
+        chatbotreply = result.fulfillmentText
+        return chatbotreply
 
         //If we need to do something in the DB we have to call the DAO class
-        return conversationDAO.startConversation();
+        //return conversationDAO.startConversation();
     },
 };
