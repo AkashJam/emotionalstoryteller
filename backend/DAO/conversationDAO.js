eed01982ec153c6cd982e98e30ef1add524939bf
AkashJam <akashaloysius.james@gmail.com>
2021-01-26 22:52:02 +0100

    Removed constrain of detecting emotion in pairs

--- backend/DAO/conversationDAO.js
+++ backend/DAO/conversationDAO.js
@@ -34,5 +34,38 @@ module.exports = {
             disgust: disgust,
             anger: anger
         }
-    }
+    },
+    // storyAssests: async (emotion) => {
+    //     try {
+    //         await database.select('story_event).where('emotion',emotion).from('story').then(function(data) {
+    //             //console.log(data[0])
+    //             result = data[0]
+    //             sug = result.suggestions
+    //             img = result.image_urls
+    //             sadness = result.sadness,
+    //             joy = result.joy,
+    //             fear = result.fear,
+    //             disgust = result.disgust,
+    //             anger = result.anger
+    //         })
+    //       }
+    //     catch(err) {
+    //         sug = null
+    //         img = null
+    //         sadness = null
+    //         joy = null
+    //         fear = null
+    //         disgust = null
+    //         anger = null
+    //     }      
+    //     // console.log(result)
+    //     return {suggestion: sug,
+    //         image_url: img,
+    //         sadness: sadness,
+    //         joy: joy,
+    //         fear: fear,
+    //         disgust: disgust,
+    //         anger: anger
+    //     }
+    // }
 };
