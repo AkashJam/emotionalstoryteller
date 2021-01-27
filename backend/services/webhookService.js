b2d492934f7e0c3f94a5978f026525357bb98159
AkashJam <akashaloysius.james@gmail.com>
2021-01-27 10:29:35 +0100

    New service in webhook for list of stories table

--- backend/services/webhookService.js
+++ backend/services/webhookService.js
@@ -29,7 +29,9 @@ module.exports = {
                 } catch (error) {
                     eventname = null
                 }
-                usertext = ''
+                if(eventname!=null){
+                    usertext = ''
+                }
             }
         }
         else{
