de052be61107d86ecfe833acd63b7060a996cd04
aida <aida@csuitecircle.com>
2021-01-30 22:36:07 +0100

    [BE] Added environments and changed knexfile location

---
+++ backend/knexfile.js
@@ -0,0 +1,33 @@
+// Update with your config settings.
+require('dotenv').config({ path: './.env' })
+
+let DBConnection;
+
+const environment = process.env.ES_ENV
+
+switch (environment) {
+  case "production": {
+    DBConnection = process.env.DATABASE_URL;
+    break;
+  }
+  case "development": {
+    DBConnection = {
+      host: process.env.DEV_DB_HOST,
+      database: process.env.DEV_DB_NAME,
+      user: process.env.DEV_DB_USR,
+      port: process.env.DEV_DB_PORT,
+      password: process.env.DEV_DB_PWD
+    }
+    break;    
+  }
+}
+
+module.exports = {
+  client: 'pg',
+  connection: DBConnection,
+  cwd: "./",
+  migrations: {
+    directory: './DB/migrations'
+  },
+  seeds: { directory: './DB/seeds' }
+};
